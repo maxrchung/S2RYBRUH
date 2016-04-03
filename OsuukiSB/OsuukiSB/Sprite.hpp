b9112cc7e02586828c2b39935865b910e9d5164e
clayton <ctbonigut@outlook.com>
2016-04-03 01:35:42 -0700

    Add _V as ScaleVector and add scaleVector to sprites

--- OsuukiSB/OsuukiSB/Sprite.hpp
+++ OsuukiSB/OsuukiSB/Sprite.hpp
@@ -15,12 +15,14 @@ public:
 	// Conflicts with below function
 	Color color;
 
-	Sprite(const std::string& filePath, Vector2 position, Layer layer = Layer::Foreground, Origin origin = Origin::Centre);
-	void Move(int startTime, int endTime, int startX, int startY, int endX, int endY, Easing easing = Easing::Linear);
+	Sprite(const std::string& filePath, Vector2 position = Vector2(320.0f, 240.0f), Layer layer = Layer::Foreground, Origin origin = Origin::Centre);
+	void Move(int startTime, int endTime, float startX, float startY, float endX, float endY, Easing easing = Easing::Linear);
 	void Move(int startTime, int endTime, Vector2 startPos, Vector2 endPos, Easing easing = Easing::Linear);
 	void Fade(int startTime, int endTime, float startOpacity, float endOpacity, Easing easing = Easing::Linear);
 	void Rotate(int startTime, int endTime, float startRotate, float endRotate, Easing easing = Easing::Linear);
 	void Scale(int startTime, int endTime, float startScale, float endScale, Easing easing = Easing::Linear);
+	void ScaleVector(int startTime, int endTime, float startX, float startY, float endX, float endY, Easing easing = Easing::Linear);
+	void ScaleVector(int startTime, int endTime, Vector2 startScale, Vector2 endScale, Easing easing = Easing::Linear);
 	void Color(int startTime, int endTime, Color startColor, Color endColor, Easing easing = Easing::Linear);
 	void Color(int startTime, int endTime, int startR, int startG, int startB, int endR, int endG, int endB, Easing easing = Easing::Linear);
 	void Write(std::ofstream& outputFile);
@@ -30,6 +32,7 @@ public:
 	Vector2 position;
 	float rotation;
 	float scale;
+	Vector2 scaleVector;
 	// Indicates when the sprite will no longer be on screen
 	int endTime;
 private:
